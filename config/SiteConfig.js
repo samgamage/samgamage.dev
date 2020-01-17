2b6f5136cb1b3e376cbfd17059feca7b877fcd8b
samgamage <sammygamage@gmail.com>
2020-01-17 17:02:16 -0500

    refactor(config): use Date() to get year in copyright statement

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -14,25 +14,25 @@ const config = {
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   userTwitter: "https://twitter.com/sambgamage", // Optionally renders "Follow Me" in the UserInfo segment.
-  copyright: "Copyright © 2019. Samuel Gamage", // Copyright string for the footer of the website and RSS feed.
-}
+  copyright: `Copyright © ${new Date().getFullYear}. Samuel Gamage` // Copyright string for the footer of the website and RSS feed.
+};
 
 // Validate
 
 // Make sure pathPrefix is empty if not needed
 if (config.pathPrefix === "/") {
-  config.pathPrefix = ""
+  config.pathPrefix = "";
 } else {
   // Make sure pathPrefix only contains the first forward slash
-  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
 }
 
 // Make sure siteUrl doesn't have an ending forward slash
 if (config.siteUrl.substr(-1) === "/")
-  config.siteUrl = config.siteUrl.slice(0, -1)
+  config.siteUrl = config.siteUrl.slice(0, -1);
 
 // Make sure siteRss has a starting forward slash
 if (config.siteRss && config.siteRss[0] !== "/")
-  config.siteRss = `/${config.siteRss}`
+  config.siteRss = `/${config.siteRss}`;
 
-module.exports = config
+module.exports = config;
