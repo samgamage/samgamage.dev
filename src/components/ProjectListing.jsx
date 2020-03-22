04297ba8302ebdf7d28f3abd1a373915baab6deb
samgamage <sammygamage@gmail.com>
2020-03-21 23:53:51 -0400

    refactor: use functional component for project listing

--- src/components/ProjectListing.jsx
+++ src/components/ProjectListing.jsx
@@ -5,62 +5,60 @@ import React from "react";
 import { jsx, Styled } from "theme-ui";
 import NewAlert from "./NewAlert";
 
-class ProjectListing extends React.Component {
-  getProjectList() {
-    const postList = [];
+function ProjectListing() {
+  const getProjectList = () => {
+    const projectList = [];
     this.props.postEdges.forEach(postEdge => {
-      postList.push({
+      projectList.push({
         path: postEdge.node.fields.slug,
         title: postEdge.node.frontmatter.title,
         description: postEdge.node.fields.description
       });
     });
 
-    return postList.filter(
+    return projectList.filter(
       post => post.title !== "filler" || post.title === ""
     );
-  }
+  };
 
-  render() {
-    const projectList = this.getProjectList();
+  const projectList = getProjectList();
 
-    return (
-      <React.Fragment>
-        {projectList.length > 0 ? (
-          projectList.map(project => {
-            const newest = dayjs(project.date) > dayjs().subtract(1, "months");
+  return (
+    <React.Fragment>
+      {projectList.length > 0 ? (
+        projectList.map(project => {
+          const newest = dayjs(project.date) > dayjs().subtract(1, "months");
 
-            return (
-              <React.Fragment>
-                <div
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between"
-                  }}
-                >
-                  <Styled.h2>
-                    <Styled.a
-                      as={Link}
-                      to={project.path}
-                      key={project.title}
-                      sx={{ textDecoration: "underline", color: "text" }}
-                    >
-                      {project.title}
-                    </Styled.a>
-                  </Styled.h2>
-                  {newest && <NewAlert />}
-                </div>
-                <Styled.p>{project.description}</Styled.p>
-              </React.Fragment>
-            );
-          })
-        ) : (
-          <Styled.p>No posts yet</Styled.p>
-        )}
-      </React.Fragment>
-    );
-  }
+          return (
+            <React.Fragment>
+              <div
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between"
+                }}
+              >
+                <Styled.h2>
+                  <Styled.a
+                    as={Link}
+                    to={project.path}
+                    key={project.title}
+                    sx={{ textDecoration: "underline", color: "text" }}
+                  >
+                    {project.title}
+                  </Styled.a>
+                </Styled.h2>
+                {newest && <NewAlert />}
+              </div>
+              <Styled.p>{project.description}</Styled.p>
+            </React.Fragment>
+          );
+        })
+      ) : (
+        <Styled.p>No posts yet</Styled.p>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default ProjectListing;
